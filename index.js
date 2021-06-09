f1943c76ba994193a2a2adea8099675cfda9664f
Peter Roto <peterdroto@gmail.com>
2021-06-08 20:48:24 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//import modules
+const inquirer = require('inquirer')
+
+//link local JS
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
